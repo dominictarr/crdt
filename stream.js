7fda7ea3e44ec32fac7d69b91397849717ed5cc9
Dominic Tarr <dominic.tarr@gmail.com>
2012-05-01 17:38:43 +1200

    quick hack to enable multiple client, single server. this will need refactoring to support multiple peer to peer connections

--- stream.js
+++ stream.js
@@ -1,19 +1,24 @@
 
 var Stream = require('stream').Stream
 var crdt = require('./index')
+var utils = require('./utils')
+
+var clone = utils.clone
+
 module.exports = 
 function create (set, name) {
   return createStream(set || new crdt.GSet('set'), name)
 }
 
-
+var _id = 0
 function createStream(set, name) {
 
   if(!set)
     throw new Error('expected a collection CRDT')
   var s = new Stream()
+  s._id = _id ++
   var sequence = 1
-  //s.set = set
+  //s.set = seex kt
   var queued = false
   var queue = []
   s.readable = s.writable = true
@@ -28,16 +33,30 @@ function createStream(set, name) {
     })
     while(hist.length)
       queue.push(hist.shift())        
-    
+
+    console.log('ENQUEUED HISTORY', queue)    
+
     set.on('flush', function (updates) {
+      console.log('-->FLUSHED')
       updates.forEach(function (e) {
         queue.push(e)
-      })
+      }) 
+      process.nextTick(s.flush)
     })
+
+  //emit data that has 
+  set.on('written', function (update, _id) {
+    console.log('WRITTEN', update, _id, s._id)
+    if(_id == s._id) return
+    queue.push(update)
+    process.nextTick(s.flush)
+  })
 
    //got to defer writing the histroy,
     //because there may still be more downstream
     //pipes that are not connected yet!
+
+    process.nextTick(s.flush)
 
     return dest
   }
@@ -51,9 +70,8 @@ function createStream(set, name) {
     while(queue.length) { 
       //this is breaking stuff in tests, because references are shared
       //with the test
-      var update = queue.shift().slice()
+      var update = clone(queue.shift())
       if(update) {
-        update[0] = update[0].slice()
         update.push(sequence++) // append sequence numbers for this oregin
         console.log('>data>', name, update)
         s.emit('data', update)
@@ -69,10 +87,18 @@ function createStream(set, name) {
     process.nextTick(s.flush)
   })
 
+/*
+******************************
+WRITES FROM OTHER NODES MUST BE WRITTEN TO ALL LISTENERS.
+
+
+******************************
+*/
+
   s.write = function (update) {
     // [path, time, update]
     // hard code only one Set right now.
-
+    var _update = clone(update)
     update[0].shift()
 
     set.update(update)
@@ -85,6 +111,12 @@ function createStream(set, name) {
       which adds the update to histroy and returns it.
 
     */
+
+    //emit this so that other connections from this CRDT
+    //and emit.
+    //man, am doing a lot of this copying...
+    set.emit('written', _update, s._id)
+
     return true
   }
 
