d54a37ca7ca1cd110f4b8de231ee4186fa2923b1
Dominic Tarr <dominic.tarr@gmail.com>
2012-05-01 14:27:54 +1200

    enqueue histroy on .pipe()

--- stream.js
+++ stream.js
@@ -2,11 +2,12 @@
 var Stream = require('stream').Stream
 var crdt = require('./index')
 module.exports = 
-function create (set) {
-  return createStream(set || new crdt.GSet('set'))
+function create (set, name) {
+  return createStream(set || new crdt.GSet('set'), name)
 }
 
-function createStream(set) {
+
+function createStream(set, name) {
 
   if(!set)
     throw new Error('expected a collection CRDT')
@@ -14,36 +15,63 @@ function createStream(set) {
   var sequence = 1
   //s.set = set
   var queued = false
+  var queue = []
   s.readable = s.writable = true
+  s.pipe = function (stream) {
+
+    var dest = Stream.prototype.pipe.call(this, stream)
+
+    //and now write the histroy!
+    var hist = set.history()
+    hist.sort(function (a, b) { 
+      return a[2] - b[2]
+    })
+    while(hist.length)
+      queue.push(hist.shift())        
+    
+    set.on('flush', function (updates) {
+      updates.forEach(function (e) {
+        queue.push(e)
+      })
+    })
+
+   //got to defer writing the histroy,
+    //because there may still be more downstream
+    //pipes that are not connected yet!
+
+    return dest
+  }
 
   s.flush = function () {
-    console.log('will flush?', queued)
-    if(!queued) return
-    var updates = set.flush()
-    console.log(updates)
-    if(!updates.length)
-      throw new Error('NO UPDATES?')
-    while(updates.length) { 
-      var update = updates.shift()
+    //if(!queue.length) 
+    set.flush()//force a flush
+    if(!queue.length)
+      return
+
+    while(queue.length) { 
+      //this is breaking stuff in tests, because references are shared
+      //with the test
+      var update = queue.shift().slice()
       if(update) {
+        update[0] = update[0].slice()
         update.push(sequence++) // append sequence numbers for this oregin
+        console.log('>data>', name, update)
         s.emit('data', update)
       }
     }
+    
     queued = false
   }
 
   set.on('queue', function () {
-    if(queued) return
-    queued = true
-    //  process.nextTick(s.flush)
+    console.log('QUEUE')
+    if(queue.length) return
+    process.nextTick(s.flush)
   })
-
 
   s.write = function (update) {
     // [path, time, update]
     // hard code only one Set right now.
-    console.log('###', update)
 
     update[0].shift()
 
