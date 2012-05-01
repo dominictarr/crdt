9fe188cd2cea718bf823f6c5ee006a8eec674eb1
Dominic Tarr <dominic.tarr@gmail.com>
2012-05-01 14:25:41 +1200

    empty queue on flush!

--- gset.js
+++ gset.js
@@ -2,6 +2,7 @@ module.exports = GSet
 
 var EventEmitter = require('events').EventEmitter
 var Obj = require('./obj')
+
 /*
   GSet -- grow only set.
 
@@ -16,28 +17,28 @@ function GSet(id) {
 }
 
 GSet.prototype = new EventEmitter()
+
 /*
   this should apply to any 
 */
+
 GSet.prototype.set =
 GSet.prototype.add = function (key, oKey, val) {
+
+  var self = this
+  function enqueue (obj) {
+    if(~self.queue.indexOf(obj)) return
+    self.queue.push(obj)
+    self.emit('queue')
+  }
+  
   if(!this.objects[key]) {
-    var self = this
     var obj = this.objects[key] = new Obj(key)
     this.state[key] = obj.get()
-    obj.on('queue', function () {
-      //prepare to flush the changes to this object
-      self.enqueue(obj)
-    })
+    obj.on('queue', function () { enqueue (obj) })
   }
   this.objects[key].set(oKey, val)
   
-}
-
-GSet.prototype.enqueue = function (obj) {
-  if(!~this.queue.indexOf(obj))
-    this.queue.push(obj)
-  this.emit('queue')
 }
 
 /*
@@ -48,19 +49,36 @@ GSet.prototype.enqueue = function (obj) {
 GSet.prototype.flush = function (obj) {
   var id = this.id
   var updates = []
-  this.queue.forEach(function (e) {
-    var update = e.flush()
+  var queue = this.queue
+  while(queue.length) {
+    //note: an object MAY NOT be a member of more than one set.
+    var update = queue.shift().flush()
     if(!update) return
     update = update.slice()
     update[0].unshift(id)
     updates.push(update)
-  })
+  }
+  this.emit('flush', updates)
   return updates
 }
 
+GSet.prototype.history = function () {
+  var hist = []
+  var objects = this.objects
+  var id = this.id
+  for(var k in objects)
+    objects[k].history().forEach(function (e) {
+      e = e.slice()
+      e[0].unshift(id)
+        
+      hist.push(e)
+    })
+  return hist
+}
+
 GSet.prototype.update = function (update) {
-  console.log('::::: UPDATE', update, this.name)
-  var key = update[0].shift()
+  update = update.slice()
+  var key = (update[0] = update[0].slice()).shift()
   var array = this.array
   if(!this.objects[key]) {
     this.state[key] = (this.objects[key] = new Obj(key)).get()
@@ -70,6 +88,7 @@ GSet.prototype.update = function (update) {
   //all that can happen is creation.
   obj.update(update)
   this.emit('update', key, this.state[key])
+
 /*
 // DELETES. move this to Set.
 //
