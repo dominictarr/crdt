c6435fca01c3f0453eaec8b162b39835e266b76b
Dominic Tarr <dominic.tarr@gmail.com>
2012-06-13 04:42:20 +1200

    sync to localStorage

--- example/socket.io/client.js
+++ example/socket.io/client.js
@@ -3,12 +3,31 @@
 var crdt    = require('crdt')
 var _bs = require('browser-stream')
 var bs = _bs(io.connect('http://localhost:3000'))
+var kv = require('kv')('crdt_example')
 
 var createChat = require('./chat')
 var createMice = require('./mouses')
 var createSets = require('./sets')
 
-var doc = new crdt.Doc()
+var doc = DOC = new crdt.Doc()
+
+function sync(doc, name) {
+  function write () {
+    doc.createReadStream({end: false}) //track changes forever
+      .pipe(kv.put(name))   
+  }
+  kv.has(name, function (err) {
+    if(err) { //the doc is new
+      doc.sync = true
+      return write() 
+    }
+    var stream = kv.get(name)
+    stream.once('end', write)
+      .pipe(doc.createWriteStream())
+  })
+}
+
+sync(doc, 'DOC')
 
 $(function () {
   var stream = crdt.createStream(doc)
