c4239a368e7999224baf539e2bccb0c5aff43acc
Dominic Tarr <dominic.tarr@gmail.com>
2012-06-14 00:16:11 +1200

    allow set creation after document load

--- test/sets.js
+++ test/sets.js
@@ -41,3 +41,47 @@ exports['test'] = function (t) {
   console.log('passed')
   t.end()
 }
+
+exports['test - post'] = function (t) {
+
+  var doc = new crdt.Doc()
+
+  doc.add({id: 'a', type: 'thing', what: 3})
+  doc.add({id: 'b', type: 'thing', what: 5})
+  doc.add({id: 'a', type: 'other', what: 7})
+  doc.add({id: 'c', type: 'thing', what: 9})
+
+  var set = doc.createSet('type', 'thing')
+  var set2 = doc.createSet('type', 'other')
+
+  function log(set) {
+
+    set.on('add', function (row) {
+      console.log('add', set.value,'->', row.state)
+    })
+    set.on('remove', function (row) {
+      console.log('rm', set.value,'->', row.state)
+    })
+
+  }
+
+  log(set)
+  log(set2)
+
+  console.log(set.toJSON())
+  console.log(set2.toJSON())
+
+  a.deepEqual(set.toJSON(), [
+    {id: 'b', type: 'thing', what: 5},
+    {id: 'c', type: 'thing', what: 9},
+  ])
+
+  a.deepEqual(set2.toJSON(), [
+    {id: 'a', type: 'other', what: 7}
+  ])
+
+  console.log('passed')
+  t.end()
+}
+
+
