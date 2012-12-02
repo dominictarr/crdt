11ff70e0a9a26dfe5437395a14f7960c6ea207fe
James Halliday <mail@substack.net>
2012-12-02 01:15:44 -0800

    failing test when inserting with an id already declared

---
+++ test/insert_with_id.js
@@ -0,0 +1,19 @@
+
+var crdt = require('..')
+var assert = require('assert')
+
+exports.test = function (t) {
+  var doc = new (crdt.Doc)
+
+  var obj = {id: 'abc', prop: 'key'}
+
+  doc.on('create', function (row) {
+    assert.deepEqual(row.toJSON(), { id: 'abc', prop: 'key' })
+    assert.deepEqual(obj, {id: 'abc', prop: 'key'})
+  })
+
+  var id = doc.insert(obj)
+  assert.equal(id, 'abc')
+
+  t.end()
+}
